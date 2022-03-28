279b9a38cce7a2ca0a77d0774cde54c54414bf18
manimohan8991 <manimohan.arichandiran@adcltech.com>
2022-03-28 17:57:46 +0530

    trend chart

--- src/app/Components/maintenancedb/maintenancedb.component.ts
+++ src/app/Components/maintenancedb/maintenancedb.component.ts
@@ -63,7 +63,10 @@ for(let j=0;j<this.servoload.length;j++){
       }
     }],
     data: {
-      labels: [],
+      labels: [
+ 
+      ],
+      
       datasets: [{
         data: [25, 25, 50],
         needleValue: this.servoload[j],
@@ -79,6 +82,9 @@ for(let j=0;j<this.servoload.length;j++){
         padding: {
           bottom: 3
         }
+      },
+      tooltips: {
+        enabled: false,
       },
       rotation: -Math.PI,
       cutoutPercentage: 30,
@@ -96,6 +102,125 @@ for(let j=0;j<this.servoload.length;j++){
 }
 }, 1000);
     
+
+var gaugeOptions = {
+  chart: {
+      type: 'solidgauge'
+  },
+
+  title: null,
+
+  pane: {
+      center: ['50%', '85%'],
+      size: '140%',
+      startAngle: -90,
+      endAngle: 90,
+      background: {
+          backgroundColor:
+              Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+          innerRadius: '60%',
+          outerRadius: '100%',
+          shape: 'arc'
+      }
+  },
+
+  exporting: {
+      enabled: false
+  },
+
+  tooltip: {
+      enabled: false
+  },
+
+  // the value axis
+  yAxis: {
+      stops: [
+          [0.1, '#55BF3B'], // green
+          [0.5, '#DDDF0D'], // yellow
+          [0.9, '#DF5353'] // red
+      ],
+      lineWidth: 0,
+      tickWidth: 0,
+      minorTickInterval: null,
+      tickAmount: 2,
+      title: {
+          y: -70
+      },
+      labels: {
+          y: 16
+      }
+  },
+
+  plotOptions: {
+      solidgauge: {
+          dataLabels: {
+              y: 5,
+              borderWidth: 0,
+              useHTML: true
+          }
+      }
+  }
+};
+
+// The speed gauge
+Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+  yAxis: {
+      min: 0,
+      max: 200,
+      title: {
+          text: 'Speed'
+      }
+  },
+
+  credits: {
+      enabled: false
+  },
+
+  series: [{
+      name: 'Speed',
+      data: [80],
+      dataLabels: {
+          format:
+              '<div style="text-align:center">' +
+              '<span style="font-size:25px">{y}</span><br/>' +
+              '<span style="font-size:12px;opacity:0.4">km/h</span>' +
+              '</div>'
+      },
+      tooltip: {
+          valueSuffix: ' km/h'
+      }
+  }]
+
+}));
+
+// The RPM gauge
+Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
+  yAxis: {
+      min: 0,
+      max: 5,
+      title: {
+          text: 'RPM'
+      }
+  },
+
+  series: [{
+      name: 'RPM',
+      data: [1],
+      dataLabels: {
+          format:
+              '<div style="text-align:center">' +
+              '<span style="font-size:25px">{y:.1f}</span><br/>' +
+              '<span style="font-size:12px;opacity:0.4">' +
+              '* 1000 / min' +
+              '</span>' +
+              '</div>'
+      },
+      tooltip: {
+          valueSuffix: ' revolutions/min'
+      }
+  }]
+
+}));
   }
 
   chart(val){
