ddea7ac38ad5684468d5a10d651c986bad7c3993
manimohan8991 <manimohan.arichandiran@adcltech.com>
2022-04-20 14:57:07 +0530

    maintenance work

--- src/app/Components/maintenancedb/maintenancedb.component.ts
+++ src/app/Components/maintenancedb/maintenancedb.component.ts
@@ -27,6 +27,10 @@ export class MaintenancedbComponent implements OnInit {
   alname: any=[];
   servoload1: any=[];
   servoloadaxis: any=[];
+  
+  servoload2: any=[];
+  servoload3: any=[];
+  maintenancedetails: any=[];
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
@@ -48,9 +52,11 @@ export class MaintenancedbComponent implements OnInit {
       }
     }
   }
-  servoload2: any=[];
-  servoload3: any=[];
-  maintenancedetails: any=[];
+  loads: any;
+  temp: any;
+  programnumber: any;
+  powerontime: any;
+  runtime: any;
   constructor(private nav:NavbarService,private route:ActivatedRoute,private service: DashboardService) {
 
 
@@ -64,15 +70,37 @@ export class MaintenancedbComponent implements OnInit {
         this.fline = res[0].line;
         this.fname = res[0].machine;
         this.myLoader = true;
-        this.service.pie(this.fline,this.fname).pipe(untilDestroyed(this)).subscribe(res=>{
-          this.myLoader=false
-          console.log(this.alname)
-        })
-        this.maintenancedetails=[]
+        // this.service.pie(this.fline,this.fname).pipe(untilDestroyed(this)).subscribe(res=>{
+         
+        //   console.log(this.alname)
+        // })
+        
         this.alname = res;
         this.service.machinedetails(this.alname[0].machine).pipe(untilDestroyed(this)).subscribe(res=>{
          this.maintenancedetails=res
+         this.myLoader=false
+         this.programnumber=this.maintenancedetails['PROGRAMNUMBER'][0].value
+         this.powerontime=this.maintenancedetails['POWERONTIME'][0].value
+         this.runtime=this.maintenancedetails['RUNTIME'][0].value
+        
          console.log(this.maintenancedetails)
+          this.loads=[]
+          this.temp=[]
+          if(Array.isArray(this.maintenancedetails['SPINDLE'])){
+
+            this.maintenancedetails['SPINDLE'].find(x => {
+              if(x['axis'] == "Spindle Load"){
+                this.loads.push(x);
+              }
+              else{
+                this.temp.push(x);
+              }
+            });
+           
+            console.log(this.loads);
+            console.log(this.temp);
+          }
+         console.log(this.loads)
          this.servoloads()
         })
     })
@@ -172,6 +200,81 @@ servoloads(){
         gauge.appendTo('#rangeContainers'+i);
       }
     }, 1000);
+
+
+
+    setTimeout(() => {
+      for(let j=0;j<this.temp.length;j++){
+        new Chart('oilCharts'+j, {
+          type: 'doughnut',
+          plugins: [{
+            afterDraw: chart => {
+              var needleValue = chart.chart.config.data.datasets[0].needleValue;
+              var dataTotal = chart.chart.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
+              var angle = Math.PI + (1 / dataTotal * needleValue * Math.PI);
+              var ctx = chart.chart.ctx;
+              var cw = chart.chart.canvas.offsetWidth;
+              var ch = chart.chart.canvas.offsetHeight;
+              var cx = cw / 2;
+              var cy = ch - 6;
+        
+            ctx.translate(cx, cy);
+            ctx.rotate(angle);
+            ctx.beginPath();
+            ctx.moveTo(0, -3);
+            ctx.lineTo(ch - 40, 0);
+            ctx.lineTo(0, 3);
+            ctx.fillStyle = 'rgb(256, 256, 256)';
+            ctx.fill();
+            ctx.rotate(-angle);
+            ctx.translate(-cx, -cy);
+            ctx.beginPath();
+            ctx.arc(cx, cy, 5, 0, Math.PI * 2);
+            ctx.fill();
+            }
+          }],
+          data: {
+            labels: [
+       
+            ],
+            
+            datasets: [{
+              data: this.temp[j].color_code,
+              borderWidth: 0,
+              needleValue: this.temp[j].value,
+              backgroundColor: [
+                '#96d617',
+                '#f2f207',
+                '#f21a07'
+              ]
+            }]
+          },
+          options: {
+            layout: {
+              padding: {
+                bottom: 3
+              },
+            },
+            tooltips: {
+              enabled: false,
+            },
+            
+            rotation: -Math.PI,
+            cutoutPercentage: 50,
+            circumference: Math.PI,
+            legend: {
+              display:false,
+              position: 'left'
+            },
+            animation: {
+              animateRotate: false,
+              animateScale: true
+            }
+          }
+        });
+      }
+      }, 1000);
+
 }
 
   ngOnInit() {  
@@ -187,87 +290,63 @@ servoloads(){
    
 
  
-setTimeout(() => {
-  for(let j=0;j<this.servoload1.length;j++){
-    new Chart('oilCharts'+j, {
-      type: 'doughnut',
-      plugins: [{
-        afterDraw: chart => {
-          var needleValue = chart.chart.config.data.datasets[0].needleValue;
-          var dataTotal = chart.chart.config.data.datasets[0].data.reduce((a, b) => a + b, 0);
-          var angle = Math.PI + (1 / dataTotal * needleValue * Math.PI);
-          var ctx = chart.chart.ctx;
-          var cw = chart.chart.canvas.offsetWidth;
-          var ch = chart.chart.canvas.offsetHeight;
-          var cx = cw / 2;
-          var cy = ch - 6;
-    
-        ctx.translate(cx, cy);
-        ctx.rotate(angle);
-        ctx.beginPath();
-        ctx.moveTo(0, -3);
-        ctx.lineTo(ch - 40, 0);
-        ctx.lineTo(0, 3);
-        ctx.fillStyle = 'rgb(256, 256, 256)';
-        ctx.fill();
-        ctx.rotate(-angle);
-        ctx.translate(-cx, -cy);
-        ctx.beginPath();
-        ctx.arc(cx, cy, 5, 0, Math.PI * 2);
-        ctx.fill();
-        }
-      }],
-      data: {
-        labels: [
-   
-        ],
-        
-        datasets: [{
-          data: [25, 25, 50],
-          borderWidth: 0,
-          needleValue: this.servoload1[j],
-          backgroundColor: [
-            '#96d617',
-            '#f2f207',
-            '#f21a07'
-          ]
-        }]
-      },
-      options: {
-        layout: {
-          padding: {
-            bottom: 3
-          },
-        },
-        tooltips: {
-          enabled: false,
-        },
-        
-        rotation: -Math.PI,
-        cutoutPercentage: 50,
-        circumference: Math.PI,
-        legend: {
-          display:false,
-          position: 'left'
-        },
-        animation: {
-          animateRotate: false,
-          animateScale: true
-        }
-      }
-    });
-  }
-  }, 1000);
+
 
   }
 
+  colorcode(value1,value2){
+
+    if(value1[0]>=value2){
+
+     return '#16a300'
+
+    }else if(value1[0]+value1[1]>=value2){
+
+      return '#ffdf00'
+
+    }else{
+      return '#ff0f0f'
+    }
+    // if(20>value2){
+
+    //   return '#ffffff'
+ 
+    //  }else if(50>value2){
+ 
+    //    return '#ffdf00'
+ 
+    //  }else{
+    //    return '#ff0f0f'
+    //  }
+  }
   getmachinedet(machinename){
     // machinedetails
     this.myLoader=true
     this.service.machinedetails(machinename).pipe(untilDestroyed(this)).subscribe(res=>{
       this.maintenancedetails=[]
-    this.maintenancedetails=res
+      this.loads=[]
+      this.temp=[]
+    this.maintenancedetails=res  
+    this.programnumber=this.maintenancedetails['PROGRAMNUMBER'][0].value
+    this.powerontime=this.maintenancedetails['POWERONTIME'][0].value
+    this.runtime=this.maintenancedetails['RUNTIME'][0].value
+   if(Array.isArray(this.maintenancedetails['SPINDLE'])){
+
+        this.maintenancedetails['SPINDLE'].find(x => {
+          if(x['axis'] == "Spindle Load"){
+            this.loads.push(x);
+          }
+          else{
+            this.temp.push(x);
+          }
+        });
+       
+        console.log(this.loads);
+        console.log(this.temp);
+      }
+    //  console.log(this.loads)
     this.myLoader=false
+   
     this.servoloads()
     })
   }
