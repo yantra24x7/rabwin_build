8e5678b6104c5d1506de82796b1d0c6eda8a4dc6
manimohan8991 <manimohan.arichandiran@adcltech.com>
2022-05-13 16:04:29 +0530

    main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,17 +4,10 @@
 export const environment = {
   production: false,
   version: '1.0.0',
-  //serverUrl: 'http://182.72.104.66:4456/api/v1/',
-   //serverUrl: 'http://13.235.142.89:81/api/v1/',
-  //  serverUrl: 'http://192.168.1.166:3000//api/v1/',
-  // serverUrl: 'http://3.7.120.8:3000//api/v1/',
-  // serverUrl: 'http://172.24.4.83:3001/api/v1/',
   serverUrl:'http://10.172.0.103:3001/api/v1/'
-  // serverUrl: 'http://192.168.2.230:3000//api/v1/',
-   //serverUrl: 'http://192.168.1.191:3001//api/v1/',
-   //serverUrl: 'http://65.0.250.235:3000//api/v1/',
-
-    //serverUrl: 'http://15.207.212.223:3000//api/v1/',
 };
-// http://103.44.98.132:3001/
 
+
+
+
+  
